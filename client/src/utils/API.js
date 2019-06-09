dc6cc62dbd739d72b763c5b39f1d51309b760d48
amandamurillo <amandasmurillo@gmail.com>
2019-06-09 07:56:33 -0700

    added front end routes, deleted create react app files

---
+++ client/src/utils/API.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default {
+    //gets books from Google API
+  getBooks: function(q) {
+      return axios.get("/api/google", {params: { q: "title" + q} });
+  }, 
+  //gets all saved books
+  getSavedBooks: function() {
+      return axios.get("/api/books");
+  }, 
+  //delete book with given id
+  delelteBook: function(id) {
+      return axios.delete("/api/books/"+ id);
+  }, 
+  //saves book to database
+  saveBook: function(bookData) {
+      return axios.post("/api/books", bookData);
+  }
+};
