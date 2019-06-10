d4464616f001c4f0d1c10882c338e4f15c7dcba0
amandamurillo <amandasmurillo@gmail.com>
2019-06-10 16:56:05 -0700

    react router to pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,45 @@
 import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
 import { Col, Row, Container } from "../components/Grid";
 
 
 class Home extends Component {
 
+state = {
 
-    render() {
-        return (
-            
+}
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col size="md-12">
+            <Jumbotron>
+              <h1 className="text-center">
+                <strong>Google Reads</strong>
+              </h1>
+              <h2 className="text-center"> Search for and Save Books </h2>
+            </Jumbotron>
+          </Col>
+          <Col size="md-12">
+            {/* /card for book search */}
+                {/* /form for search */}
+
+          </Col>
+        </Row>
+
+        <Row>
+          <Col size="md-12">
+          {/* card with results */}
+          {/* if results, return Card > List > Book */}
+        {/* else return a please search message */}
+
+
+          </Col>
+        </Row>
+      </Container>
+
         )
     }
 }
+
+export default Home;
