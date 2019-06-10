bf4d7583b36f6d8abd77962f8d417ad519323697
amandamurillo <amandasmurillo@gmail.com>
2019-06-10 15:46:33 -0700

    Jumbotron component added and no match route page added

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Col, Row, Container } from "../components/Grid";
+
+
+class Home extends Component {
+
+
+    render() {
+        return (
+            
+        )
+    }
+}
